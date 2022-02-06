8a93b33015f4e3f03d85de66e687f19e6ec2f603
Suliman Fayyaz <hydrajin@gmail.com>
2022-02-05 23:37:01 -0500

    fix user signup bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,9 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
+    if User.find_by(email: user.email)
+      redirect_to "/login"
+    elsif user.save
       session[:user_id] = user.id
       redirect_to "/"
     else
