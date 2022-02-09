47d97f1d60564047059acba7a0650748e07b5c3b
Suliman Fayyaz <hydrajin@gmail.com>
2022-02-08 19:40:53 -0500

    add user-spec with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
   has_secure_password
-  # validates :name, presence: true
-  # validates :email, presence: true
-  # validates :password_digest, presence: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 4 }
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.strip.downcase
+    user = User.find_by_email(email)
+    if user.nil? || !user.authenticate(password)
+      nil
+    else
+      user
+    end
+  end
 end
+
+# https://stackoverflow.com/questions/38899698/how-to-get-proper-email-uniqueness-in-rspec
+# https://stackoverflow.com/questions/13111328/testing-password-length-validation-with-rspec
